918527dc451d2975ae8f01694d7d6123741ee2bb
Bowen Suico <78730-18443219@SERVER2.DOMAIN>
2024-05-11 12:08:09 +0800

    first commit

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -2,11 +2,43 @@ import { useEffect, useState } from 'react';
 import { db } from "../../configs/firebase";
 import { doc, getDoc } from 'firebase/firestore';
 import { useParams, Link } from 'react-router-dom';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe('pk_test_51PF6II08fB3svemS23bJNsoFfqys8gBus2wHDqpUgM2is6AAZrY8Ft5jrQTDQd9OgTlJoMWN3Mu5fYUC2bJ0nXuT006xVUxgLy'); // Replace with your publishable key
 
 function ProductDetails() {
     const { id } = useParams();
     const [product, setProduct] = useState(null);
 
+    // Handle the click event when the user clicks the "Checkout" button
+  const handleClick = async (product_name, product_price) => {
+    const stripe = await stripePromise;
+
+    // Send a request to the backend to create a checkout session
+    const response = await fetch('http://localhost:4000/create-checkout-session', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ product_name, product_price }), // Send product name and price to the backend
+    });
+
+    if (response.ok) {
+      // If the request is successful, retrieve the session ID from the response
+      const session = await response.json();
+
+      // Redirect the user to the Stripe Checkout page using the session ID
+      const result = await stripe.redirectToCheckout({ sessionId: session.id });
+
+      if (result.error) {
+        // If there is an error during the redirect, display the error message
+        setError(result.error.message);
+      }
+    } else {
+      // If there is an error creating the checkout session, display an error message
+      setError('Error creating checkout session');
+    }
+  };
     useEffect(() => {
         const getProductDetails = async () => {
             try {
@@ -25,6 +57,9 @@ function ProductDetails() {
         getProductDetails();
     }, [id]);
 
+    
+
+
     return (
         <div className='item'>
             <Link to='/'><button className='button'>Back</button></Link>
@@ -34,7 +69,7 @@ function ProductDetails() {
                     <h1>Item Name: {product.product_name}</h1>
                     <h2>Description: {product.product_description}</h2>
                     <h2>Price: {product.product_price}</h2>
-                    
+                    <button onClick={()=>handleClick(product.product_name, product.product_price*100)}>Checkout</button>
                 </div>
             ) : (
                 <p>Loading product details...</p>
